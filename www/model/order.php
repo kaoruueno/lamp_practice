6277cb7d97191cab78c5c4b1b25b86856fef2d28
Your Name <you@example.com>
2020-07-19 23:29:32 +0900

    訂正後

--- www/model/order.php
+++ www/model/order.php
@@ -13,22 +13,24 @@ function regist_order($db, $user_id) {
     return execute_query($db, $sql, $params);
 }
 
-function regist_order_details($db, $item_name, $price, $amount) {
+function regist_order_details($db, $order_id, $item_name, $price, $amount) {
     $params = [
+        $order_id,
         $item_name,
         $price,
         $amount
     ];
-    $sql = 'INSERT INTO order_details (name, price, amount) 
-            VALUES (?, ?, ?)';
+    $sql = 'INSERT INTO order_details (order_id, name, price, amount) 
+            VALUES (?, ?, ?, ?)';
     return execute_query($db, $sql, $params);
 }
 
 function regist_purchase_carts_transaction($db, $user, $carts) {
     $db->beginTransaction();
     if (purchase_carts($db, $carts) && regist_order($db, $user['user_id'])) {
+        $order_id = $dbh->lastInsertId('order_id');
         foreach ($carts as $cart) {
-            if (regist_order_details($db, $cart['name'], $cart['price'], $cart['amount']) === false) {
+            if (regist_order_details($db, $order_id, $cart['name'], $cart['price'], $cart['amount']) === false) {
                 $db->rollback();
                 return false;
             }
